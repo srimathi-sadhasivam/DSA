06577d5959f6401027cbd3921fcd99dae8433e22
Srimathi sadhasivam <srimathisadhasivam@gmail.com>
2026-08-05 09:04:15 +0530

    Time: 347 ms (14.12%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,1 +1,1 @@
-select max(salary) as SecondHighestSalary from Employee where salary<(select max(salary) from Employee);
+select max(salary) as SecondHighestSalary from Employee where salary <(select max(salary) from Employee);
